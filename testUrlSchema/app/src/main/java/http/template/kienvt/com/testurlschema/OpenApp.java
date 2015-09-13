d7e61a80293b51fc714f1490e858b045771bde94
MrKien <mrkien@MrKiens-MacBook-Pro.local>
2015-09-13 22:53:23 +0700

    first

---
+++ testUrlSchema/app/src/main/java/http/template/kienvt/com/testurlschema/OpenApp.java
@@ -0,0 +1,40 @@
+package http.template.kienvt.com.testurlschema;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class OpenApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_app);
+        TextView tv = (TextView)findViewById(R.id.testTV);
+        tv.setText(getIntent().getData().getQueryParameter("from"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_open_app, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
